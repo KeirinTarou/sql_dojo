9f05aed5cfe4919401e643e0b86cb5766fdeef96
Keirin Tarou <seoo_japan_keirinoh@yahoo.co.jp>
2026-02-23 12:31:01 +0900

    [Update]db/connection.py
    - SQLiteデータベースに対応

--- db/connection.py
+++ db/connection.py
@@ -1,21 +1,30 @@
 # import mysql.connector
 import pyodbc
 import os
+from pathlib import Path
 
 def get_connection_string():
-    return (
-        f"DRIVER={ os.getenv("DB_DRIVER") };"
-        f"SERVER={ os.getenv('DB_SERVER') };"
-        f"PORT={ os.getenv('DB_PORT') };"
-        f"DATABASE={ os.getenv('DB_DATABASE') };"
-        f"UID={ os.getenv('DB_USER') };"
-        f"PWD={ os.getenv('DB_PASSWORD') };"
-        "OPTION=3;"
-    )
+    if os.getenv("USE_ODBC") == "USE_MYSQL":
+        return (
+            f"DRIVER={ os.getenv("MYSQL_DB_DRIVER") };"
+            f"SERVER={ os.getenv('MYSQL_DB_SERVER') };"
+            f"PORT={ os.getenv('MYSQL_DB_PORT') };"
+            f"DATABASE={ os.getenv('MYSQL_DB_DATABASE') };"
+            f"UID={ os.getenv('MYSQL_DB_USER') };"
+            f"PWD={ os.getenv('MYSQL_DB_PASSWORD') };"
+            "OPTION=3;"
+        )
+    elif os.getenv("USE_ODBC") == "USE_SQLITE":
+        DB_PATH = Path(__file__).parent / os.getenv("SQLITE_DB_NAME")
+        return (
+            f"DRIVER={ os.getenv("SQLITE_DB_DRIVER") };"
+            f"DATABASE={ DB_PATH };"
+        )
 
 def get_connection():
     """`pyodbc.Connection`インスタンスを返す
     """
+    print(get_connection_string())
     return pyodbc.connect(get_connection_string())
 
 # mysql.connector版
